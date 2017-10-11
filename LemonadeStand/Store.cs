7d1a3903ef58763d9ac107c3bec5845761cec480
beigenfeld <beigenfeld@gmail.com>
2017-10-11 16:52:18 -0500

    Completed Store functionality

--- LemonadeStand/Store.cs
+++ LemonadeStand/Store.cs
@@ -43,7 +43,7 @@ namespace LemonadeStand
 
         public void BuySupplies(Player player)
         {
-            Console.WriteLine("Would you like to buy:\n[1] Cups\n[2] Lemons\n[3]Sugar\n[4] Ice\n[5] Nothing more.  I'm all set to sell some lemonade!");
+            Console.WriteLine("Would you like to buy:\n[1] Cups\n[2] Lemons\n[3] Sugar\n[4] Ice\n[5] Check Inventory\n[6] Nothing more.  I'm all set to sell some lemonade!");
             string userInput = Console.ReadLine();
             
             switch (userInput)
@@ -54,13 +54,17 @@ namespace LemonadeStand
                 case "2":
                     BuyLemons(player);
                     break;
-                //case "3":
-                //    BuySugar();
-                //    break;
-                //case "4":
-                //    BuyIce();
-                //    break;
-                //case "5":
+                case "3":
+                    BuySugar(player);
+                    break;
+                case "4":
+                    BuyIce(player);
+                    break;
+                case "5":
+                    DisplayInventory(player);
+                    BuySupplies(player);
+                    break;
+                //case "6":
                 //    //OpenForBusiness();
                 //    break;
                 default:
@@ -99,7 +103,7 @@ namespace LemonadeStand
             }
             if (CheckIfEnough(price, player.inventory.cashOnHand) == false)
             {
-                Console.WriteLine("Ummmm, you seem to be a little short...");
+                Console.WriteLine("Ummmm, you seem to be a little short... You only have $" + player.inventory.cashOnHand);
                 BuyCups(player);
             }
             else
@@ -143,7 +147,7 @@ namespace LemonadeStand
             }
             if (CheckIfEnough(price, player.inventory.cashOnHand) == false)
             {
-                Console.WriteLine("Ummmm, you seem to be a little short...");
+                Console.WriteLine("Ummmm, you seem to be a little short... You only have $" + player.inventory.cashOnHand);
                 BuyLemons(player);
             }
             else
@@ -156,65 +160,88 @@ namespace LemonadeStand
                 BuySupplies(player);
             }
         }
-        //void BuySugar()
-        //{
-        //    Console.WriteLine("Would you like to buy\n[1] 10 cups of Sugar for $1.00\n[2] 20 cups of Sugar for $1.75\n[3] 50 cups of Sugar for $4.00\n[4] No more sugar for me, I'm sweet enough!");
-        //    string userInput = Console.ReadLine();
-        //    double price = 0;
-        //    switch (userInput)
-        //    {
-        //        case "1":
-        //            price = 1;
-        //            //CheckIfEnough(price, player.inventory.cashOnHand);
-        //            sugarOnHand += sugarOnHand + 10;
-        //            cashOnHand -= price;
-        //            break;
-        //        case "2":
-        //            //CheckIfEnough();
-        //            sugarOnHand += sugarOnHand + 20;
-        //            cashOnHand -= cashOnHand - 1.75;
-        //            break;
-        //        case "3":
-        //            //CheckIfEnough();
-        //            sugarOnHand += sugarOnHand + 50;
-        //            cashOnHand -= cashOnHand - 4;
-        //            break;
-        //        case "4":
-        //            break;
-        //        default:
-        //            Console.WriteLine("Sorry, we don't carry that here.  Please type in '1' '2' '3' or '4'");
-        //            BuySugar();
-        //            break;
-        //    }
-        //}
-        //void BuyIce()
-        //{
-        //    Console.WriteLine("Would you like to buy\n[1] 10 cubes of Ice for $.50\n[2] 20 cubes of Ice for $.90\n[3] 50 cubes of Ice for $2.00\n[4] Brrrr, it's cold in here! Let's get out of the freezer!");
-        //    string userInput = Console.ReadLine();
-        //    switch (userInput)
-        //    {
-        //        case "1":
-        //            //CheckIfEnough();
-        //            iceOnHand += iceOnHand + 10;
-        //            cashOnHand -= cashOnHand - .5;
-        //            break;
-        //        case "2":
-        //            //CheckIfEnough();
-        //            iceOnHand += iceOnHand + 20;
-        //            cashOnHand -= cashOnHand - .9;
-        //            break;
-        //        case "3":
-        //            //CheckIfEnough();
-        //            iceOnHand += iceOnHand + 50;
-        //            cashOnHand -= cashOnHand - 2;
-        //            break;
-        //        case "4":
-        //            break;
-        //        default:
-        //            Console.WriteLine("Sorry, we don't carry that here.  Please type in '1' '2' '3' or '4'");
-        //            BuyIce();
-        //            break;
-        //    }
-        //}
+        void BuySugar(Player player)
+        {
+            Console.WriteLine("Would you like to buy\n[1] 10 cups of Sugar for $1.00\n[2] 20 cups of Sugar for $1.75\n[3] 50 cups of Sugar for $4.00\n[4] No more sugar for me, I'm sweet enough!");
+            string userInput = Console.ReadLine();
+            double price = 0;
+            int quantity = 0;
+            switch (userInput)
+            {
+                case "1":
+                    price = 1;
+                    quantity = 10;
+                    break;
+                case "2":
+                    price = 1.75;
+                    quantity = 20;
+                    break;
+                case "3":
+                    price = 4;
+                    quantity = 50;
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Sorry, we don't carry that here.  Please type in '1' '2' '3' or '4'");
+                    BuySugar(player);
+                    break;
+            }
+            if (CheckIfEnough(price, player.inventory.cashOnHand) == false)
+            {
+                Console.WriteLine("Ummmm, you seem to be a little short... You only have $" + player.inventory.cashOnHand);
+                BuySugar(player);
+            }
+            else
+            {
+                player.inventory.sugarOnHand += quantity;
+                player.inventory.cashOnHand -= price;
+                DisplayInventory(player);
+                Console.WriteLine("Anything else?");
+                BuySupplies(player);
+            }
+        }
+        void BuyIce(Player player)
+        {
+            Console.WriteLine("Would you like to buy\n[1] 10 cubes of Ice for $.50\n[2] 20 cubes of Ice for $.90\n[3] 50 cubes of Ice for $2.00\n[4] Brrrr, it's cold in here! Let's get out of the freezer!");
+            string userInput = Console.ReadLine();
+            double price = 0;
+            int quantity = 0;
+            switch (userInput)
+            {
+                case "1":
+                    price = .5;
+                    quantity = 10;
+                    break;
+                case "2":
+                    price = .9;
+                    quantity = 20;
+                    break;
+                case "3":
+                    price = 2;
+                    quantity = 50;
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Sorry, we don't carry that here.  Please type in '1' '2' '3' or '4'");
+                    BuyIce(player);
+                    break;
+            }
+            if (CheckIfEnough(price, player.inventory.cashOnHand) == false)
+            {
+                Console.WriteLine("Ummmm, you seem to be a little short... You only have $" + player.inventory.cashOnHand);
+                BuyIce(player);
+            }
+            else
+            {
+                player.inventory.iceOnHand += quantity;
+                player.inventory.cashOnHand -= price;
+                DisplayInventory(player);
+                Console.WriteLine("Anything else?");
+                BuySupplies(player);
+            }
+            
+        }
     }
 }
